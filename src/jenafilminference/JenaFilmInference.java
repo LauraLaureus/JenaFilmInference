c71db80f7d5c21e2a0fa0c56ebbef07baa5e5687
Laura <l4depende@gmail.com>
2016-04-21 08:35:51 +0100

    Base

---
+++ src/jenafilminference/JenaFilmInference.java
@@ -0,0 +1,16 @@
+package jenafilminference;
+
+/**
+ *
+ * @author lala
+ */
+public class JenaFilmInference {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
