19e15576344ab00345364ce9caf4262d3f3ae956
Laura <l4depende@gmail.com>
2016-04-21 09:20:04 +0100

    Java Structure

--- src/jenafilminference/JenaFilmInference.java
+++ src/jenafilminference/JenaFilmInference.java
@@ -1,5 +1,18 @@
 package jenafilminference;
 
+import org.apache.jena.rdf.model.InfModel;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.reasoner.Reasoner;
+import org.apache.jena.reasoner.ReasonerRegistry;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.util.PrintUtil;
+import org.apache.jena.vocabulary.RDF;
+
 /**
  *
  * @author lala
@@ -10,7 +23,22 @@ public class JenaFilmInference {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Model scheme = RDFDataMgr.loadModel("scheme.ttl");
+        Model data = RDFDataMgr.loadModel("data.ttl");
+        
+        Reasoner reasoner = ReasonerRegistry.getRDFSSimpleReasoner();
+        InfModel inference = ModelFactory.createInfModel(reasoner, scheme,data);
+        
+        String ns = "urn:si2:film";
+        
+        StmtIterator iter;
+        Resource juan = inference.getResource(ns+"juan");
+        iter = inference.listStatements(juan,RDF.type ,(RDFNode)null);
+        while(iter.hasNext()){
+            Statement statement = iter.nextStatement();
+            System.out.println("--->" + PrintUtil.print(statement));
+        }
     }
     
 }
